0ad25622bb0d1c36f0b30db7802a3e437afc7540
Mukul Mangla <mukul.mangla@t-mobile.com>
2022-03-30 01:25:16 -0700

    Task 4: Ability to mark a book as finished

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -12,3 +12,31 @@ describe('When: I use the reading list feature', () => {
     );
   });
 });
+describe('When: I use the reading list feature', () => {
+  beforeEach(() => {
+    cy.startAt('/');
+  });
+
+  it('Then: I should see my reading list', () => {
+    cy.get('[data-testing="toggle-reading-list"]').click();
+
+    cy.get('[data-testing="reading-list-container"]').should(
+      'contain.text',
+      'My Reading List'
+    );
+  });
+
+  it('Then: I should be able to mark the book as finished', () => {
+    cy.get('input[type="search"]').type('angular');
+
+    cy.get('form').submit();
+
+    cy.get('[data-testing="add-item"]:enabled').first().click();
+
+    cy.get('[data-testing="toggle-reading-list"]').click();
+
+    cy.get('[data-testing="finish-book-cta"]:enabled').first().click();
+
+    cy.get('[data-testing="finish-date"]').first().should('contain.text', 'Finished reading');
+  });
+});
